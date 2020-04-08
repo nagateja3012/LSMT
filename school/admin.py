e0cbdb014b2e7457ca52c04bf9dbae58c8b39278
nagateja3012 <52905466+nagateja3012@users.noreply.github.com>
2020-04-08 15:34:16 +0530

    Update admin.py

--- school/admin.py
+++ school/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from school.models import Dashboard,Profile
+from school.models import Dashboard
 # Register your models here.
 
 admin.site.register(Dashboard)
-admin.site.register(Profile)
